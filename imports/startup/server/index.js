368e8fc811ebcae77c73dc4ea85e6ad4af47462e
Piotr Starzec <piotr.starzec@fortrus.com>
2016-08-25 16:19:04 +0200

    Added WebStorm run configurations

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -7,5 +7,5 @@ import '../../api/cars/cars';
 Meteor.startup(() => {
 	// code to run on server at startup
 	Log.info('Server started in', process.env.NODE_ENV, 'mode');
-	Log.debug('Debug mode is working');
+	Log.debug('Debug is working in development mode');
 });
